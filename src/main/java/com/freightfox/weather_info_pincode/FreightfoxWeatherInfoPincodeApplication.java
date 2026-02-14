248f2b8def8aa5af38d0bdd4108b6dea3c8feb75
tejuingalagi <tejuingalagi07@gmail.com>
2026-02-14 22:12:42 +0530

    feat: create Project setup and WeatherInfoEntity for storing weather data by pincode

---
+++ src/main/java/com/freightfox/weather_info_pincode/FreightfoxWeatherInfoPincodeApplication.java
@@ -0,0 +1,13 @@
+package com.freightfox.weather_info_pincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightfoxWeatherInfoPincodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightfoxWeatherInfoPincodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freightfox/weather_info_pincode/FreightfoxWeatherInfoPincodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freightfox.weather_info_pincode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightfoxWeatherInfoPincodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
