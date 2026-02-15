ef9a5df554da3905adf47d0f0c8f08dd0777f187
tejuingalagi <tejuingalagi07@gmail.com>
2026-02-15 17:57:21 +0530

    refactor: update weather API endpoint to accept date parameter

--- src/main/java/com/freightfox/weather_info_pincode/controller/WeatherController.java
+++ src/main/java/com/freightfox/weather_info_pincode/controller/WeatherController.java
@@ -4,7 +4,6 @@ import com.freightfox.weather_info_pincode.dto.WeatherRequest;
 import com.freightfox.weather_info_pincode.dto.WeatherResponse;
 import com.freightfox.weather_info_pincode.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,7 +16,7 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @PostMapping
-    public ResponseEntity<WeatherResponse> getWeather(@Valid @RequestBody WeatherRequest request) {
-        return ResponseEntity.ok(weatherService.getWeather(request));
+    public WeatherResponse getWeather(@Valid @RequestBody WeatherRequest request) {
+        return weatherService.getWeather(request);
     }
 }
